888c6958aba75b81a77140bdd1aefbb9bd3baa07
reoli <domondongeron2@gmail.com>
2025-05-03 13:40:48 +0700

    task table headers added

--- inventory/projectTypes.ts
+++ inventory/projectTypes.ts
@@ -1,15 +1,15 @@
 import { Schema as S } from 'effect'
 
 export const TaskStatus = S.Union( 
-    S.Literal('todo'),
-    S.Literal('ongoing'),
-    S.Literal('done')
+    S.TaggedStruct('todo', {}),
+    S.TaggedStruct('ongoing', {}),
+    S.TaggedStruct('done', {})
 )
 
 export const Priority = S.Union(
-    S.Literal('low'),
-    S.Literal('medium'),
-    S.Literal('high')
+    S.TaggedStruct('low', {}),
+    S.TaggedStruct('medium', {}),
+    S.TaggedStruct('high', {})
 )
 
 export const Task = S.TaggedStruct('Task', {
@@ -17,12 +17,26 @@ export const Task = S.TaggedStruct('Task', {
     title: S.String,
     status: TaskStatus,
     priority: Priority,
+    assignee: S.Union( S.String, S.Null )
 })
 
 export const TaskBoard = S.TaggedStruct('TaskBoard', {
     name: S.String,
-    board: S.Array(Task)
+    board: S.HashMap({
+        key: S.String,
+        value: Task
+    })
+})
+
+export const TaskCollectionStats = S.TaggedStruct('TaskCollectionStats', {
+    total: S.Number, 
+    done: S.Number, 
+    inProgress: S.Number, 
+    todo: S.Number 
 })
 
 export type Task = typeof Task.Type
 export type TaskBoard = typeof TaskBoard.Type
+export type TaskStatus = typeof TaskStatus.Type
+export type Priority = typeof Priority.Type
+export type TaskCollectionStats = typeof TaskCollectionStats.Type
--- inventory/taskScript.ts
+++ inventory/taskScript.ts
@@ -27,15 +27,43 @@ function main() {
     const taskNameInput = document.createElement('input')
         taskNameInput.defaultValue = 'New task '
     const priorityDropdown = setupPriorityDropdown()
+    const addTaskButton = document.createElement('button')
+        addTaskButton.textContent = 'Add Task'
+
+    const taskBoard = TaskBoard.make({
+        name: 'Daily Tasks',
+        board: HashMap.empty()
+    })
 
     taskNameInput.addEventListener('click', () => clearTextboxIfDefaultVal(taskNameInput))
     taskNameInput.addEventListener('focusout', () => revertToDefaultValueIfEmpty(taskNameInput))
+    
 
     root.append(taskNameInput)
     root.append(priorityDropdown)
+    root.append(addTaskButton)
+
+    root.append(makeTasksTable(taskBoard))
 
     
     
+}
+
+function makeTasksTable(taskBoard: TaskBoard): HTMLTableElement {
+    const statsTable = document.createElement('table')
+    statsTable.border = '1'
+    statsTable.style = 'border-collapse: collapse;'
+        const statsTableHeader = document.createElement('thead')
+            const headerRow = document.createElement('tr')
+            for (const columnHead of Array.make('ID', 'Name', 'Priority', 'Status')) {
+                const headerCell = document.createElement('th')
+                headerCell.textContent = columnHead
+                headerRow.appendChild(headerCell)
+            }
+        statsTableHeader.appendChild(headerRow)
+    statsTable.appendChild(statsTableHeader)
+
+    return statsTable
 }
 
 const revertToDefaultValueIfEmpty = (textbox: HTMLInputElement) => {
